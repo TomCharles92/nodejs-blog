32edbd0c40b206736a2af93df1e2b0117b18f51e
TomCharles92 <b8%a37@UPQKl>
2020-07-05 18:10:18 +0800

    express 处理 session

--- blog-express/app.js
+++ blog-express/app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
 
 // 引用路由
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 const blogRouter = require('./routes/blog')
 const userRouter = require('./routes/user')
 
@@ -15,8 +16,8 @@ var app = express();
 
 // view engine setup
 // 前端页面的初始化，后端可以不用管
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // 插件 morgan，用来记录日志
 // 这里会记录访问日志？
@@ -30,11 +31,20 @@ app.use(express.urlencoded({
 // 处理之后，req.cookies 可以直接访问 cookie
 app.use(cookieParser());
 // 前端页面相关，静态文件
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: '3^yrXS!R61jFsna$',
+  cookie: {
+    // path: '/', // 默认值
+    // httpOnly: true, // 默认值
+    maxAge: 24 * 60 * 60 * 10000
+  }
+}))
 
 /* 注册路由 */
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 app.use('/api/blog', blogRouter)
 app.use('/api/user', userRouter)
 
--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -1,12 +1,36 @@
 const express = require('express')
 const router = express.Router()
+const {
+  getList,
+  getDetail,
+  newBlog,
+  updateBlog,
+  delBlog
+} = require('../controller/blog')
+const {
+  SuccessModel,
+  ErrorModel
+} = require('../model/resModel')
 
 router.get('/list', function (req, res, next) {
-  // 1. 以 json 格式返回数据
-  // 2. 添加 headers Content-Type: application/json;
-  res.json({
-    errno: 0,
-    data: [12, 123, 232]
+  let {
+    author = '', keyword = ''
+  } = req.query
+
+  // if (query.isadmin) {
+  //   // 管理员界面
+  //   const loginCheckResult = loginCheck(req)
+  //   if (loginCheckResult) {
+  //     // 未登录
+  //     return loginCheckResult
+  //   }
+  //   // 查询自己的博客
+  //   author = req.session.username
+  // }
+
+  const result = getList(author, keyword)
+  return result.then(listData => {
+    res.json(new SuccessModel(listData, '这是获取博客列表的接口'))
   })
 })
 
