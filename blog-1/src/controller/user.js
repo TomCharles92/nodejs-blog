21bd793523fe4fad12e0b541b82ea8c07e869f67
TomCharles92 <b8%a37@UPQKl>
2020-07-05 11:51:42 +0800

    密码加密

--- blog-1/src/controller/user.js
+++ blog-1/src/controller/user.js
@@ -2,9 +2,11 @@ const {
   exec,
   escape
 } = require('../db/mysql')
+const { genPassword } = require('../utils/cryp')
 
 const login = (username, password) => {
   username = escape(username)
+  password = genPassword(password)
   password = escape(password)
   
   const sql = `select username, realname from users 
