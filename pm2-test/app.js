67321be4200b64fd8ce862954cfb6d3eb68d4871
TomCharles92 <b8%a37@UPQKl>
2020-07-08 21:58:10 +0800

    pm2: 配置文件

--- pm2-test/app.js
+++ pm2-test/app.js
@@ -14,7 +14,7 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-type', 'application/json')
   res.end(JSON.stringify({
     errno: 0,
-    msg: 'pm2 test server 1'
+    msg: 'pm2 test server 2'
   }))
 })
 
